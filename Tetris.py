b1d04b0bf80fa00e2f4d21ccbd2ceb3b3d6ee3a6
Stochastis <calebsross@gmail.com>
2021-10-26 08:17:27 -0500

    Created __init__ method for Tetris class. Creates new lines in the game field according to the height and width.

--- Tetris.py
+++ Tetris.py
@@ -34,3 +34,12 @@ class Tetris:
     y = 60
     zoom = 20
     figure = None
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        for i in range(height):
+            new_line = []
+            for j in range(width):
+                new_line.append(0)
+            self.field.append(new_line)
