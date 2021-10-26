8a74c8f352553084c20e40932ecc4f43fcc4382d
Stochastis <calebsross@gmail.com>
2021-10-26 08:07:52 -0500

    Created __init__ function for Figure class. Randomly pickes a type of tetromino and a color.

--- Tetris.py
+++ Tetris.py
@@ -1,5 +1,5 @@
 class Figure:
-    figures = [
+    figures = [ #Contains all 7 tetrominos and their rotations. All in a 4x4 grid.
         [[1, 5, 9, 13], [4, 5, 6, 7]], #'I' piece
         [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]], #'L' piece
         [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]], #'J' piece
@@ -8,3 +8,10 @@ class Figure:
         [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]], #'T' piece
         [[1, 2, 5, 6]], #'O' piece
     ]
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.type = random.randint(0, len(self.figures) - 1)
+        self.color = random.randint(1, len(colors) - 1)
+        self.rotation = 0
