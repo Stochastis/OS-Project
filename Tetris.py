12915ea3851b6958714d712b227dd8f702db8949
ulpiana <ulpiana.b@hotmail.com>
2021-11-04 09:12:14 -0500

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -1,3 +1,5 @@
+import pygame 
+from pygame.locals import *
 class Figure:
     figures = [ #Contains all 7 tetrominos and their rotations. All in a 4x4 grid.
                 #TODO: Verify that rotations are in order (clockwise or counter-clockwise) according to Tetris game development rules.
@@ -22,7 +24,10 @@ class Figure:
 
     def rotate(self):
         self.rotation = (self.rotation + 1) % len(self.figures[self.type])
-
+'''
+Initialize game. Create field of height and width and create a new tetromino
+positioned at (3,0)
+'''
 class Tetris:
     level = 2
     score = 0
@@ -34,7 +39,6 @@ class Tetris:
     y = 60
     zoom = 20
     figure = None
-
     def __init__(self, height, width):
         self.height = height
         self.width = width
@@ -43,7 +47,13 @@ class Tetris:
             for j in range(width):
                 new_line.append(0)
             self.field.append(new_line)
-
+    def new_figure(self):
+        self.figure = Figure(3, 0)
+    '''
+    Check if the tetromino flying down is going to clash with anything fixed on the field.
+    The for loops go through a 4x4 matrix, checking if the tetromino is out of bounds
+    and if it's touching a busy game field. If there is a zero, then we're all good and the field is empty
+    '''
     def intersects(self):
         intersection = False
         for i in range(4):
@@ -52,3 +62,146 @@ class Tetris:
                     if i+self.figure.y > self.height-1 or j+self.figure.x > self.width - 1 or j+self.figure.x < 0 or self.field[i+self.figure.y][j+self.figure.x] > 0:
                         intersection = True
         return intersection
+    '''
+    When a tetromino reaches the bottom, we freeze the field so that the controls are no
+    longer focused on that piece and instead on a new one flying down
+    '''
+    def freeze(self):
+        for i in range(4):
+            for j in range(4): # Set up matrix
+                if i * 4 + j in self.figure.image():
+                    self.field[i+self.figure.y][j+self.figure.x] = self.figure.color
+                    self.break_lines()
+                    self.new_figure()
+                    if self.intersects(): # If tetromino interesects with field
+                        game.state = "gameover"
+    '''
+    Check if there are any lines that are filled that we need to destroy.
+    Destroying a line goes from bottom to top
+    '''
+    def break_lines(self):
+        lines = 0
+        for i in range(1, self.height):
+            zeros=0
+            for j in range(self.width):
+                if self.field[i][j] == 0:
+                    zeros +=1
+            if zeros == 0:
+                lines += 1
+                for i1 in range(i,1,-1):
+                    for j in range(self.width):
+                        self.field[i1][j] = self.field[i1 - 1][j]
+                self.score += lines **2
+    ''' 
+    The moving methods. In every method we remember the last position and if an
+    intersection already exists. If there is an intersection in the tetrominoes, return to previous state
+    '''
+    def go_space(left):
+        while not self.intersects():
+            self.figure.y += 1
+        self.figure.y -= 1
+        self.freeze()
+    def go_down(self):
+        self.figure.y += 1
+        if self.intersects():
+            self.figure.y -= 1
+            self.freeze()
+    def go_side(self,dx):
+        old_x = self.figure.x
+        self.figure.x += dx
+        if self.intersects():
+            self.figure.x = old_x
+    def rotate(self):
+        old_rotation = self.figure.rotation
+        self.figure.rotate()
+        if self.intersects():
+            self.figure.rotation = old_rotation
+    
+    '''
+    oui oui initalízê le gâme engine hoh hoh hoh
+    (change this later)
+    '''
+    pygame.init()
+
+    '''
+    définir des couleurs
+    '''
+    BLACK = (0,0,0)
+    WHITE = (255,255,255)
+    GRAY = (128,128,128)
+
+    size = (400,500)
+    screen = pygame.display.set_mode(size)
+
+    pygame.display.set_caption("SHOW ME WHAT YOU GOT")
+
+    '''
+    Boucle jusqu'à ce que l'utilisateur clique sur fermer
+    '''
+    done = False
+    clock = pygame.time.Clock()
+    fps = 25
+    game  = Tetris(20,10)
+    counter = 0
+
+    pressing_down = False
+
+    while not done:
+        if game.figure is None:
+            game.new_figure()
+        counter += 1
+        if counter > 100000:
+            counter = 0
+        if counter % (fps // game.level // 2) == 0 or pressing_down:
+            if game.state == "start":
+                game.go_down()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done  = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygamee.K_UP:
+                    game.rotate()
+                if event.key == pygame.K_DOWN:
+                    pressing_down = True
+                if event.key == pygame.K_LEFT:
+                    game.go_side(-1)
+                if event.key == pygame.K_RIGHT:
+                    game.go_side(1)
+                if event.key == pygame.K_SPACE:
+                    game.go_space()
+                if event.key == pygame.K_ESCAPE:
+                    game.__init__(20,10)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                pressing_down = False
+        screen.fill(WHITE)
+
+        for i in range(game.height):
+            for j in range(game.width):
+                pygame.draw.rect(screen,GRAY, [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom],1)
+                if game.field[i][j] > 0:
+                    pygame.draw.rect(screen, colors[game.field[i][j]],
+                                 [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
+        if game.figure is not None:
+            for i in range(4):
+                for j in range(4):
+                    p = i * 4 + j
+                    if p in game.figure.image():
+                        pygame.draw.rect(screen, colors[game.figure.color],
+                                        [game.x + game.zoom *(j +game.figure.x) +1,
+                                         game.y  +game.zoom * (i +game.figure.y)+1,
+                                         game.zoom - 2, game.zoom -2])
+        font = pygame.font.SysFont('Calibri', 25, True, False)
+        font1 = pygame.font.SysFont('Calibri', 65, True, False)
+        text = font.render("Score: " + str(game.score), True, BLACK)
+        text_game_over = font1.render("Game Over", True, (255, 125,0))
+        text_game_over1 = font1.render("Press ESC", True, (255,215,0))
+
+        screen.blit(text, [0,0])
+        if game.state == "gameover":
+            screen.blit(text_game_over, [20,200])
+            screen.blit(text_game_over1, [25,265])
+        pygame.display.flip()
+        clock.tick(fps)
+    pygame.quit()
